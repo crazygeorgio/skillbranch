c135fad2b2f2c4206f06866799fa7ab600584fd9
Yuriy Krokhmal <uktrance@gmail.com>
2016-11-13 00:21:22 +0300

    reworked for all tests

--- src/index.js
+++ src/index.js
@@ -16,21 +16,26 @@ app.get('/task2A', (req, res) => {
 
 app.get('/task2B', (req, res) => {
 	const error = 'Invalid fullname',
-		regExp = new RegExp(/\d/,'g');
+		regExp = new RegExp(/[\d\_\/]/,'g'),
+		regExpS = new RegExp(/\s+/,'g');
 	let fullname = req.query.fullname,
 		fullnameArr;
 
+
+		console.log(fullname.match(regExp));
+
 	if(fullname && fullname.match(regExp) === null) {
+		fullname = fullname.trim().toLowerCase().replace(regExpS, ' ');
 		fullnameArr = fullname.split(' ');
 		switch(fullnameArr.length) {
 			case 3:
-				fullname = fullnameArr[2] + ' ' + fullnameArr[0][0] + '.' + ' ' + fullnameArr[1][0] + '.';
+				fullname = fullnameArr[2][0].toUpperCase() + fullnameArr[2].substr(1) + ' ' + fullnameArr[0][0].toUpperCase() + '.' + ' ' + fullnameArr[1][0].toUpperCase() + '.';
 				break;
 			case 2:
-				fullname = fullnameArr[1] + ' ' + fullnameArr[0][0] + '.';
+				fullname = fullnameArr[1][0].toUpperCase() + fullnameArr[1].substr(1) + ' ' + fullnameArr[0][0].toUpperCase() + '.';
 				break;
 			case 1:
-				fullname = fullnameArr[0];
+				fullname = fullnameArr[0][0].toUpperCase() + fullnameArr[0].substr(1);
 				break;
 			default:
 			 	fullname = error;
@@ -47,9 +52,13 @@ app.get('/task2C', (req, res) => {
 	let username = req.query.username,
 		result;
 
-	result = username.split('/');
+	result = username.split('//');
 
 	result = result[result.length - 1];
+
+	result = result.split('/');
+
+	result = result.length > 1 ? result[1] : result[0];
 
 	result = result.split('?');
 
