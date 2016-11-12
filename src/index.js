258cafe9ace92811e99e173dffa4cac1e881dfa3
Yuriy Krokhmal <uktrance@gmail.com>
2016-11-12 23:40:20 +0300

    task 2b done

--- src/index.js
+++ src/index.js
@@ -14,6 +14,36 @@ app.get('/task2A', (req, res) => {
 	res.send(sum.toString());
 });
 
+app.get('/task2B', (req, res) => {
+	const error = 'Invalid fullname',
+		regExp = new RegExp(/\d/,'g');
+	let fullname = req.query.fullname,
+		fullnameArr;
+
+	if(fullname && fullname.match(regExp) === null) {
+		fullnameArr = fullname.split(' ');
+		switch(fullnameArr.length) {
+			case 3:
+				fullname = fullnameArr[2] + ' ' + fullnameArr[0][0] + '.' + ' ' + fullnameArr[1][0] + '.';
+				break;
+			case 2:
+				fullname = fullnameArr[1] + ' ' + fullnameArr[0][0] + '.';
+				break;
+			case 1:
+				fullname = fullnameArr[0];
+				break;
+			default:
+			 	fullname = error;
+		}
+	} else {
+		fullname = error;
+	}
+
+
+
+	res.send(fullname);
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
